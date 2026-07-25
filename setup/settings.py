1937a2335bfae3ba8118e55bb3e6e8f89b494c30
Lucas Vasconcelos <lucasmarcelo0109@gmail.com>
2026-07-25 15:11:02 -0300

    Corrigindo arquivo requirements.

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1"
+    ".vercel.app",
+]
 
 
 # Application definition
